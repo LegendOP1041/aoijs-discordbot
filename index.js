4de342b5ec77e51c1cb95e763a0df63b2743e0de
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2023-06-11 16:08:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const { TOKEN } = require('./config.json');
+const { AoiClient, LoadCommands } = require("aoi.js");
+
+const bot = new AoiClient({
+    token: TOKEN ,
+    prefix: "_",
+    intents: ["MessageContent", "Guilds", "GuildMessages"],
+    events: ["onMessage", "onInteractionCreate"],
+    database: {
+        type: "aoi.db",
+        db: require("aoi.db"),
+        tables: ["main"],
+        path: "./database/",
+        extraOptions: {
+            dbType: "KeyValue"
+        }
+    }
+});
+
+const loader = new LoadCommands(bot);
+loader.load(bot.cmd, "./commands/")
+
+/* Slash Interaction Command Example (ping)
+You must execute the function below for the slash command to work:
+$createApplicationCommand[$guildID;ping;Pong!;true;slash]
+*/
+
+bot.interactionCommand({
+    name: "ping",
+    prototype: "slash",
+    code: `$interactionReply[Pong! $pingms;;;;everyone;false]`
+});
